28c52741483c1ef7999a01a6c408b8a82c4ed36c
nayyyyyyyy <loire.alexis2004@gmail.com>
2024-03-21 14:50:34 +0100

    Update Admin

--- Controller/insert_salarie.php
+++ Controller/insert_salarie.php
@@ -26,6 +26,35 @@ $stmt->bindParam(2, $_POST['id_service']);
 $stmt->bindParam(3, $_POST['id_salarie']);
 $result_medecin= $stmt->fetchAll();
 
+if (isset($_POST['delete_button2'])){
+    $nom_salarie = $_POST['nom_salarie'];
+    $query= "DELETE FROM `salarie` WHERE nom_salarie='$nom_salarie'";
+    $stmt = $conn->prepare($query);
+    
+   
+    echo "DELETE";
+
+    $result_salarie = $stmt->fetchAll();
+
+    $success = executeInsertion($stmt);
+
+}else if (isset($_POST['update_button2'])){
+    $nom_service = $_POST['nom_service'];
+    $query= "UPDATE `services` SET nom_service='$nom_service'";
+    $stmt = $conn->prepare($query);
+   
+    echo "UPDATE";
+
+    $result_services = $stmt->fetchAll();
+
+    $success = executeInsertion($stmt);
+
+}
+else{
+    //no button pressed
+    echo "NOTHING";
+}
+
 if ($success) {
     $_SESSION['insert_success'] = "L'ajout du médécin a bien été effectuée.";
     header('Location: ../View/pannel.php');
@@ -35,5 +64,5 @@ if ($success) {
 }
 
 
-
 ?>
+
